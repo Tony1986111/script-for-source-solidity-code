8b46adf652b95816bdc06c5cf92ebd3682b616b6
Tony <87019512+Tony1986111@users.noreply.github.com>
2024-03-07 17:15:43 +1100

    Update source_solidity_code.js

--- source_solidity_code.js
+++ source_solidity_code.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
-const API_KEY = 'ZB2TTGYQ56WW7ZE5TRDD48868SQXAQSTZC';
-const CONTRACT_ADDRESS = '0x388C818CA8B9251b393131C08a736A67ccB19297';
+const API_KEY = 'Your_Etherscan_API';
+const CONTRACT_ADDRESS = 'The_Contract_Address';
 const URL = `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${CONTRACT_ADDRESS}&apikey=${API_KEY}`;
 
 axios.get(URL)
@@ -12,26 +12,20 @@ axios.get(URL)
     const jsonContent = response.data.result[0].SourceCode;
     // console.log(jsonContent);
     const correctedJson = jsonContent.replace(/^{{/, '{').replace(/}}$/, '}');
-
     let parsedContent = JSON.parse(correctedJson);
     
-
-    // 遍历'sources'对象
+    // Traverse the 'sources' object
     for (let filePath in parsedContent.sources) {
-        // 获取合约内容
+        // Get the contract content
         let fileContent = parsedContent.sources[filePath].content;
-        // 处理文件路径，确保目录存在
+        // check the file path and make sure the directory exist
         let fullFilePath = path.join(__dirname, filePath);
         let directory = path.dirname(fullFilePath);
         if (!fs.existsSync(directory)){
             fs.mkdirSync(directory, { recursive: true });
         }
-        // 将合约内容写入对应的.sol文件
+        // write the contract to .sol file
         fs.writeFileSync(fullFilePath, fileContent, 'utf8');
         console.log(`File saved: ${filePath}`);
     }
-
-
-
-
   });
